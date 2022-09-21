273b08ebd1fa1dbf4d07041ad81fe5b6f2748afe
JFoxx64 <ragefoxx64@gmail.com>
2022-09-21 08:10:28 -0400

    ff0.18r
    Fixing the unnecessary prompt to have an uninstall button per Mozilla demands

--- Firefox Extension/pages/permissionscript.js
+++ Firefox Extension/pages/permissionscript.js
@@ -1,15 +1,52 @@
-function saveSetting(){
-  browser.runtime.sendMessage({ type : "updatepermission", message : true });
-
-  let button = document.getElementById("agreebutton");
-  if(button) button.remove();
-
-  var agreedtext = document.createElement('div');
-  agreedtext.textContent = "Thank you!  You can close this page now."
-  document.body.appendChild(agreedtext);
+let agreebutton = document.getElementById("agreebutton");
+if(agreebutton) {
+  agreebutton.addEventListener("click", function(){ saveSetting(true) });
 }
 
-let button = document.getElementById("agreebutton");
-if(button) {
-  button.addEventListener("click", saveSetting);
+let disagreebutton = document.getElementById("disagreebutton");
+if(disagreebutton) {
+  disagreebutton.addEventListener("click", function(){ saveSetting(false) });
+}
+
+let uninstallbutton = document.getElementById("uninstallbutton");
+if(uninstallbutton) {
+  function savePreference(){
+    function onCanceled(error) {
+      console.log(`Canceled: ${error}`);
+    }
+    
+    let uninstalling = browser.management.uninstallSelf({
+      showConfirmDialog: true
+    });
+    
+    uninstalling.then(function(){
+      removeButtons();
+      var agreedtext = document.createElement('div');
+      agreedtext.textContent = "Sorry to see you go, but still hope that you have a great rest of your day!<br/>You can close this tab now"
+      document.body.appendChild(agreedtext);
+    }, onCanceled);
+  }
+
+  uninstallbutton.addEventListener("click", savePreference);
+}
+
+function removeButtons(){
+  let agreebutton = document.getElementById("agreebutton");
+  let disagreebutton = document.getElementById("disagreebutton");
+  let uninstallbutton = document.getElementById("uninstallbutton");
+
+  if(agreebutton) agreebutton.remove();
+  if(disagreebutton) disagreebutton.remove();
+  if(uninstallbutton) uninstallbutton.remove();
+}
+
+function saveSetting(setting){
+  console.log(setting)
+  browser.runtime.sendMessage({ type : "updatepermission", message : setting });
+
+  removeButtons();
+
+  var agreedtext = document.createElement('div');
+  agreedtext.textContent = "Your preferences have been saved."
+  document.body.appendChild(agreedtext);
 }
