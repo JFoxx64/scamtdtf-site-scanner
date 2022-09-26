b6a28feb96acdaa2995e2e218a81cd5ddbf66d15
JFoxx64 <ragefoxx64@gmail.com>
2022-09-26 13:26:17 -0400

    cv0.20r and ev0.20r Releases
    @ Updated side "Scanner" to be a button instead that shows a colored status bar based on what is found
    + Added a new warning system for some of the more popular scam locations to help educate users even if the Deep Scanner does not detect a scam
    + Added a new Info Chip that is displayed if a warning is triggered from the new system
    @ Updated the icons to be PNG as Firefox likes to complain about that but we don't get notes from the other vendors
    @ Updated some of the various components to use classes instead of styles
    @ Oh, and the biggest update: SEPERATED THE MAIN FILE.  It was getting crowded and bloated making it difficult to read through and update.  Now every component has their own file for ease of development (and hopefully review!)
    
    ! Note: New version system will be [Release].[Major].[Minor] - Minor will be used for small updates that fix issues that get a Major update blocked on a store platform (as an example)

--- Chrome Extension/content_scripts/sitecheck.js
+++ Chrome Extension/content_scripts/sitecheck.js
@@ -1,4 +1,4 @@
-//TODO: Add ability to scan site for keywords and popout side button with warning
+let warningkey = null;
 
 function checkSite(){
     if(!checked){
@@ -17,25 +17,31 @@ function checkSite(){
             if(res.found){
                 laststatus = "warning";
                 siteid = res.data[0].id;
-                createBanner("warning", "warning-banner", "Warning!", "This website has been reported as a scam on Scam Takedown Task Force");
+                createBanner("warning", "stdtf-warning-banner", "Warning!", "This website has been reported as a scam on Scam Takedown Task Force");
                 partialHideButton();
             }else if(res.inreview){
                 laststatus = "caution";
                 if(previousstatus != laststatus){
-                    createBanner("caution", "caution-banner", "Caution!", "This website has been flagged as a scam on Scam Takedown Task Force and is awaiting Auditor review");
+                    createBanner("caution", "stdtf-caution-banner", "Caution!", "This website has been flagged as a scam on Scam Takedown Task Force and is awaiting Auditor review");
                     partialHideButton();
                 }
             }else if(res.indeepscan){
-                laststatus = "queue"
+                laststatus = "queue";
+                destroyAllBanners();
             }else if(res.userreported){
                 laststatus = "userreported"
                 if(previousstatus != laststatus){
-                    createBanner("caution", "reported-banner", "Caution!", "This website has been reported by users as a scam on Scam Takedown Task Force and is awaiting Auditor review");
+                    createBanner("caution", "stdtf-reported-banner", "Caution!", "This website has been reported by users as a scam on Scam Takedown Task Force and is awaiting Auditor review");
                     partialHideButton();
                 }
+            } else if(warningkey){
+                laststatus = "warningpattern"
+                
+                partialHideButton();
             } else{
                 laststatus = "clear"
                 destroyAllBanners();
+                partialHideButton();
             }
 
             if(laststatus != "clear" && laststatus != "warning"){
@@ -44,21 +50,75 @@ function checkSite(){
                 }, 5000);
             }
 
-            if(previousstatus == "scanning" && laststatus == "clear"){
-                partialHideButton();
-            }
-
             if(!finalcheck && laststatus == "clear"){
                 finalcheck = true;
+                checkWarnings();
+
+                if(warningkey){
+                    laststatus = "warningpattern"
+                    destroyAllBanners();
+                    partialHideButton();
+                }
 
                 setTimeout(() => {
                     checkSite();
-                    partialHideButton();
                 }, 10000);
             }
             
             updateSiteChipStatus();
+            updateSideButtonStatus();
             checked = true;
         })
-        .catch( error => console.log(error));
+        .catch( error => {
+            console.log(error);
+
+            checkWarnings();
+
+            if(warningkey){
+                laststatus = "warningpattern"
+            } else{
+                laststatus = "error"
+            }
+
+            setTimeout(() => {
+                checkSite();
+            }, 10000);
+
+            destroyAllBanners();
+            partialHideButton();
+            updateSiteChipStatus();
+            updateSideButtonStatus();
+
+            checked = true;
+        });
+}
+
+function checkWarnings(){
+    if(WARNING_KEYS.length > 0){
+        let html = document.body.innerHTML;
+        let strippedhtml = html.replace(/\s/g, '').toLowerCase();
+    
+        let maxvaluehits = null;
+        let maxhits = 0;
+    
+        for(let i = 0; i < WARNING_KEYS.length; i++){
+            let hits = 0;
+    
+            for(let j = 0; j < WARNING_KEYS[i].keys.length; j++){
+                if(strippedhtml.includes(WARNING_KEYS[i].keys[j].word)){
+                    hits += WARNING_KEYS[i].keys[j].weight;
+                }
+            }
+    
+            if(hits >= WARNING_KEYS[i].threshhold && maxhits < hits){
+                maxvaluehits = WARNING_KEYS[i];
+            }
+        }
+    
+        warningkey = maxvaluehits;
+    } else{
+        setTimeout(() => {
+            checkWarnings();
+        }, 100);
+    }
 }
